21c4e50dfd2284b42b30361ac092c9f8bfaee9af
mileoa <ezkimozzzzz@gmail.com>
2024-06-12 19:41:59 +1000

    Добавил ответ на задание.

---
+++ BigMinus/BigMinus.py
@@ -0,0 +1,42 @@
+def is_more(s1: str, s2: str) -> bool:
+    """Return whether int in s1 string more than int in s2."""
+    for i, s1_digit in enumerate(s1):
+        if s2[i] < s1_digit:
+            return True
+        if s2[i] > s1_digit:
+            return False
+    return False
+
+def BigMinus(s1: str, s2: str) -> str:
+    """Return absolute difference."""
+    # Add leading 0 to make str equal len.
+    while len(s1) > len(s2):
+        s2 = "0"+ s2
+    while len(s1) < len(s2):
+        s1 = "0"+ s1
+
+    # Decide minuend and subtrahend.
+    result: list[str] = []
+    subtrahend: str = ""
+
+    if is_more(s1, s2):
+        result = list(s1)
+        subtrahend = s2
+    else:
+        result = list(s2)
+        subtrahend = s1
+
+    # School arithmetic.
+    for i in range(len(result)-1, -1, -1):
+
+        if int(result[i]) - int(subtrahend[i]) < 0:
+            result[i - 1] = str(int(result[i - 1]) - 1)
+            result[i] = str(int(result[i]) + 10 - int(subtrahend[i]))
+            continue
+        result[i] = str(int(result[i]) - int(subtrahend[i]))
+
+    return "".join(result)
+
+
+
+
---
+++ BigMinus/BigMinusTests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from BigMinus import BigMinus
+
+class BigMinusTests(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(BigMinus("1234567891", "1"), "1234567890")
+        self.assertEqual(BigMinus("1", "321"), "320")
+        self.assertEqual(BigMinus("61", "321"), "260")
+
+    def test_border(self):
+        self.assertEqual(BigMinus("0", "321"), "321")
+        self.assertEqual(BigMinus("321", "0"), "321")
+        self.assertEqual(BigMinus("0", "0"), "0")
+
+if __name__ == '__main__':
+    unittest.main()
