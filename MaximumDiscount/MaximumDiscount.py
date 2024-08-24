7e6b8d1acc3b84901e18554a79afb03cc0ef79ab
mileoa <ezkimozzzzz@gmail.com>
2024-08-24 23:54:34 +1000

    refacotor: changed len(price) to parameter N

--- MaximumDiscount/MaximumDiscount.py
+++ MaximumDiscount/MaximumDiscount.py
@@ -1,5 +1,3 @@
 def MaximumDiscount(N: int, price: list[int]) -> int:
     """Return max discount."""
-    return sum(
-        sorted(price, reverse=True)[i] for i in range(len(price)) if (i + 1) % 3 == 0
-    )
+    return sum(sorted(price, reverse=True)[i] for i in range(N) if (i + 1) % 3 == 0)
