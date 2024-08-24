6abc40663e723ad3d1f3340404fd2c6b6c8dfce7
mileoa <ezkimozzzzz@gmail.com>
2024-08-24 23:51:27 +1000

    refacotor: rewrite function

--- MaximumDiscount/MaximumDiscount.py
+++ MaximumDiscount/MaximumDiscount.py
@@ -1,14 +1,5 @@
 def MaximumDiscount(N: int, price: list[int]) -> int:
     """Return max discount."""
-    total_discount: int = 0
-
-    sorted_price: list[int] = price[:]
-    sorted_price.sort(reverse=True)
-
-    for i in range(N//3):
-        total_discount += sorted_price[i*3 + 2]
-
-    return total_discount
-
-
-
+    return sum(
+        sorted(price, reverse=True)[i] for i in range(len(price)) if (i + 1) % 3 == 0
+    )
