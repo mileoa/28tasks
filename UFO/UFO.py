7d6e3eed47fb20f9f9f85afc678e7677d4e3688b
mileoa <ezkimozzzzz@gmail.com>
2024-09-09 22:16:15 +1000

    refactor: move result variable closer to where it is used.

--- UFO/UFO.py
+++ UFO/UFO.py
@@ -5,11 +5,9 @@ HEXADECIMAL_BASE = 16
 def list_int(num: int) -> list[int]:
     """List int by digits."""
     result: list[int] = []
-
     while num > 0:
         result.append(num % 10)
         num = num // 10
-
     result.reverse()
     return result
 
@@ -18,7 +16,6 @@ def convert_to_dec(num: int, base: int) -> int:
     """Convert to decimical."""
     digits: list[int] = list_int(num)
     result: int = 0
-
     for i, digit in enumerate(digits):
         result += digit * base ** (len(digits) - 1 - i)
     return result
@@ -26,14 +23,13 @@ def convert_to_dec(num: int, base: int) -> int:
 
 def UFO(N: int, data: list[int], octal: bool) -> list[int]:
     """Return converted UFO sygnal."""
-    result: list[int] = []
     base: int = 0
     if octal:
         base = OCTAL_BASE
     else:
         base = HEXADECIMAL_BASE
 
+    result: list[int] = []
     for i in range(N):
         result.append(convert_to_dec(data[i], base))
-
     return result
