f84a384f69f34bb495a0a7115a3387e3a82d7288
mileoa <ezkimozzzzz@gmail.com>
2024-09-11 09:02:53 +1000

    refactor: removed try except to implenet logic other way

--- Odometer/odometer.py
+++ Odometer/odometer.py
@@ -1,7 +1,15 @@
+from collections import deque
+
+
 def odometer(oksana: list[int]) -> int:
+    oksana_deque: deque[int] = deque(oksana)
     hours_passed_from_start: int = 0
     path_lenght_km: int = 0
-    for i in range(1, len(oksana), 2):
-        path_lenght_km += oksana[i - 1] * (oksana[i] - hours_passed_from_start)
-        hours_passed_from_start = oksana[i]
+    while len(oksana_deque) != 0:
+        average_speed_from_last_point_kmph: int = oksana_deque.popleft()
+        hours_passed_from_start_new: int = oksana_deque.popleft()
+        path_lenght_km += average_speed_from_last_point_kmph * (
+            hours_passed_from_start_new - hours_passed_from_start
+        )
+        hours_passed_from_start = hours_passed_from_start_new
     return path_lenght_km
