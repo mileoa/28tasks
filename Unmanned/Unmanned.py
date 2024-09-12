3df5a101fea67da921b8e0ba6411b98b7ad1f154
mileoa <ezkimozzzzz@gmail.com>
2024-09-12 23:52:39 +1000

    refactor: replace comments for boolean expressions by bool values

--- Unmanned/Unmanned.py
+++ Unmanned/Unmanned.py
@@ -7,12 +7,13 @@ def Unmanned(L: int, N: int, track: list[list[int]]) -> int:
     vehicle_pos: int = 0
     time_spent: int = 0
     while vehicle_pos < L:
-        # Car on light and light is red.
-        if (
-            # Check what part of full cicle is.
-            vehicle_pos in light_cicle
-            and time_spent % sum(light_cicle[vehicle_pos]) < light_cicle[vehicle_pos][0]
-        ):
+        is_red_light_on_vehicle_postition: bool = False
+        is_vehicle_on_the_light: bool = vehicle_pos in light_cicle
+        if is_vehicle_on_the_light:
+            is_red_light_on_vehicle_postition: bool = (
+                time_spent % sum(light_cicle[vehicle_pos]) < light_cicle[vehicle_pos][0]
+            )
+        if is_red_light_on_vehicle_postition:
             time_spent += 1
             continue
 
